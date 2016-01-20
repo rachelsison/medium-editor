c86083c77931b6a757297a9e890e092db0c48255
rachelsison <rachel@boomtrain.com>
2016-01-20 13:13:53 -0800

    WIP test

--- dist/js/medium-editor.js
+++ dist/js/medium-editor.js
@@ -1759,7 +1759,7 @@ MediumEditor.extensions = {};
             while (!stop && node) {
                 // Only iterate over elements and text nodes
                 if (node.nodeType > 3) {
-                    node = nodeStack.pop()
+                    node = nodeStack.pop();
                     continue;
                 }
 
@@ -1965,7 +1965,7 @@ MediumEditor.extensions = {};
             while (!stop && node) {
                 // Only iterate over elements and text nodes
                 if (node.nodeType > 3) {
-                    node = nodeStack.pop()
+                    node = nodeStack.pop();
                     continue;
                 }
 
